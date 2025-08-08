5b160f307795fc0b1940c6394569d5f603ac8f82
Garth Pickell <gpick0@gmail.com>
2025-08-07 17:49:59 -0700

    *** test

---
+++ .devcontainer/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+hint="${GITHUB_REPOSITORY#*/}"
+hint=$(echo "$hint" | sed -E 's/[^[:alnum:]]+/_/g' | tr '[:lower:]' '[:upper:]')
+hint="SETTINGS__${hint}"
+settings="${!hint}"
+
+cat > .env <<EOF
+$settings
+FRONTEND_URL=https://$CODESPACE_NAME-8080.$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN
+EOF
